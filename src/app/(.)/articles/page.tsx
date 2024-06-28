4afd49bd61817dfe1838aa386a2cb80a6fe1cb02
Tomas Izuel <tomasizuel@gmail.com>
2024-06-27 23:57:42 -0300

    create button

--- src/app/(.)/articles/page.tsx
+++ src/app/(.)/articles/page.tsx
@@ -1,12 +1,22 @@
 import ArticleWrapper from "@/components/clients/ArticleWrapper";
 import Await from "@/components/common/Await";
 import { getAllArticles } from "@/services/article.service";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 const ArticlePage = () => {
   const articles = getAllArticles();
   return (
     <main className=" flex flex-col gap-10">
-      <h1>Articulos</h1>
+      <header className="flex justify-between items-center">
+        <h1>Articulos</h1>
+        <Link href={"/articles/create"}>
+          <Button color="primary">
+            <PlusCircleIcon className="w-6 h-6" /> Crear
+          </Button>
+        </Link>
+      </header>
       <Await promise={articles}>
         {(articles) => <ArticleWrapper articles={articles} />}
       </Await>
